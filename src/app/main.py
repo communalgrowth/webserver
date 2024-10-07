6ea25051d8db450a8c298329fb05df12e79896af
Nikolaos Chatzikonstantinou <nchatz314@gmail.com>
2024-10-07 07:52:06 -0400

    fix indentation

--- src/app/main.py
+++ src/app/main.py
@@ -31,7 +31,6 @@ def generic_exception_handler(_: Request, exc: Exception) -> Response:
     """Default handler for exceptions subclassed from HTTPException."""
     status_code = getattr(exc, "status_code", HTTP_500_INTERNAL_SERVER_ERROR)
     detail = "Error."
-
     return Response(
         media_type=MediaType.TEXT,
         content=detail,
