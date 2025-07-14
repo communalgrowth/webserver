e01d1e5d4cc30cc4f90279258749626ddc1f0286
Nikolaos Chatzikonstantinou <nchatz314@gmail.com>
2025-07-14 05:02:15 -0400

    add subscribers to search results

--- src/app/search.py
+++ src/app/search.py
@@ -4,7 +4,7 @@ The module enabling the search functionality on the website.
 
 """
 
-from sqlalchemy import select, func, text
+from sqlalchemy import select
 from sqlalchemy.orm import selectinload
 from app.cgdb import Document
 from app.utils import strip_to_alphanum
@@ -19,12 +19,17 @@ async def search_documents(Session, search_term, limit=100):
     stmt = (
         select(Document)
         .options(selectinload(Document.authors))
+        .options(selectinload(Document.cgusers))
         .filter(Document.tsv_title.match(stripped))
         .limit(limit)
     )
     async with Session() as session:
         results = await session.execute(stmt)
     return [
-        (doc.title, ", ".join([a.author for a in doc.authors]))
+        (
+            doc.title,
+            ", ".join([a.author for a in doc.authors]),
+            ", ".join([u.email for u in doc.cgusers]),
+        )
         for doc in results.scalars().all()
     ]
